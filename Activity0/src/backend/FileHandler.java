5535255e21856dbc579fb3e14f99bbb0a08fe948
Lestat <2233915@slu.edu.ph>
2024-01-18 21:05:35 +0800

    +initial/incomplete version of FileHandler

--- Activity0/src/backend/FileHandler.java
+++ Activity0/src/backend/FileHandler.java
@@ -1,4 +1,36 @@
 package backend;
 
-public class FileHandler {
+import java.io.*;
+import java.util.Scanner;
+
+public class FileHandler<T> {
+    private Scanner scanner = null;
+
+    public FileHandler(){
+
+    }
+
+    public T[] readCSVFile(File file){
+        //insert logic of reading csv files
+
+        return null;
+    }
+
+    public T[] readCSVFile(String filePath){
+        try {
+            File file = new File(filePath);
+            scanner = new Scanner(file);
+
+
+        }catch(FileNotFoundException fileNotFound){
+            fileNotFound.printStackTrace();
+        }
+        return null;
+    }
+
+    public void saveToExcel(T[] arrayOfObject){
+        // insert logic to save file using library that utilizes excel files
+    }
+
+
 }
