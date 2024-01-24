1e7d92503742d56373c7a5954c56aa81499361bb
Lou Diamond <2233672@slu.edu.ph>
2024-01-24 08:45:30 +0800

    + Created: Main.java

---
+++ Activity0/src/Main.java
@@ -0,0 +1,9 @@
+import controllers.FileHandlerController;
+import models.AthleteModel;
+import views.GUI;
+
+public class Main {
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
