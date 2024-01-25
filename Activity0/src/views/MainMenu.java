fb44b7a4fc6fa9cf49fc2343f9087404c785e92d
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 00:00:18 +0800

    + utilized swing ui designer

---
+++ Activity0/src/views/MainMenu.java
@@ -0,0 +1,17 @@
+package views;
+
+import javax.swing.*;
+
+public class MainMenu {
+    private JComboBox comboBox1;
+    private JPanel mainPanel;
+
+    public JComboBox getComboBox1() {
+        return comboBox1;
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+}
---
+++ Activity0/src/views/SelectFile.java
@@ -0,0 +1,16 @@
+package views;
+
+import javax.swing.*;
+
+public class SelectFile {
+    private JPanel mainPanel;
+    private JButton selectFileButton;
+
+    public JButton getSelectFileButton() {
+        return selectFileButton;
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
