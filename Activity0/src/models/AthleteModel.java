81238810eb92580e807523a01f5de61b576a65d6
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 20:47:56 +0800

    + set the all attributes into object of String

--- Activity0/src/models/AthleteModel.java
+++ Activity0/src/models/AthleteModel.java
@@ -1,25 +1,25 @@
 package models;
 
 public class AthleteModel {
-    private int id, age, height, weight;
-    private char sex;
+    private String id, age, height, weight;
+    private String sex;
     private String name, team, games, city, sport, medal;
 
     /**
      * Default constructor (Null object of AthleteModel)
      */
-    public AthleteModel(){
-        id = -1;
-        age = -1;
-        height = -1;
-        name = null;
-        sex = 'z';
-        team = null;
-        games = null;
-        city = null;
-        sport = null;
-        medal = null;
-    }
+//    public AthleteModel(){
+//        id = -1;
+//        age = -1;
+//        height = -1;
+//        name = null;
+//        sex = 'z';
+//        team = null;
+//        games = null;
+//        city = null;
+//        sport = null;
+//        medal = null;
+//    }
 
     /**
      * Constructor class overload
@@ -36,8 +36,22 @@ public class AthleteModel {
      * @param sport     Sport played by the athlete
      * @param medal     Medal earned by the athlete
      */
-    public AthleteModel(int id, String name, char sex, int age, int height, int weight, String team,
-    String games, String city, String sport, String medal){
+//    public AthleteModel(int id, String name, char sex, int age, int height, int weight, String team,
+//    String games, String city, String sport, String medal){
+//        this.id = id;
+//        this.name = name;
+//        this.sex = sex;
+//        this.age = age;
+//        this.height = height;
+//        this.weight = weight;
+//        this.team = team;
+//        this.games = games;
+//        this.city = city;
+//        this.sport = sport;
+//        this.medal = medal;
+//    }
+    public AthleteModel(String id, String name, String sex, String age, String height, String weight, String team,
+                        String games, String city, String sport, String medal){
         this.id = id;
         this.name = name;
         this.sex = sex;
@@ -51,47 +65,50 @@ public class AthleteModel {
         this.medal = medal;
     }
 
-
-    // Getter and setter methods
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public char getSex() {
-        return sex;
-    }
-
-    public void setSex(char sex) {
-        this.sex = sex;
-    }
+    // Getter and setter methods
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(int height) {
+//        this.height = height;
+//    }
+//
+//    public int getWeight() {
+//        return weight;
+//    }
+//
+//    public void setWeight(int weight) {
+//        this.weight = weight;
+//    }
+//
+//    public char getSex() {
+//        return sex;
+//    }
+//
+//    public void setSex(char sex) {
+//        this.sex = sex;
+//    }
 
     public String getName() {
         return name;
@@ -139,5 +156,13 @@ public class AthleteModel {
 
     public void setMedal(String medal) {
         this.medal = medal;
+    }
+
+    @Override
+    public String toString() {
+        return id.replaceAll("\"", "")+ "," + age.replaceAll("\"", "")+ "," + height.replaceAll("\"", "")+ "," +
+                weight.replaceAll("\"", "")+ "," + sex.replaceAll("\"", "")+ "," + name.replaceAll("\"", "")+ ","
+                + team.replaceAll("\"", "")+ "," + games.replaceAll("\"", "")+ "," + city.replaceAll("\"", "")+ ","
+                + sport.replaceAll("\"", "")+ "," + medal.replaceAll("\"", "");
     }
 }
