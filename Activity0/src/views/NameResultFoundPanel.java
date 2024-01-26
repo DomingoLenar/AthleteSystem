681c2cbe04e82c5fcd76c727a233a87d1caa8107
Georcelle <2234664@slu.edu.ph>
2024-01-26 21:06:20 +0800

    initial name result found panel

---
+++ Activity0/src/views/NameResultFoundPanel.java
@@ -0,0 +1,16 @@
+package views;
+
+import javax.swing.*;
+
+public class NameResultFoundPanel {
+        private JPanel mainPanel;
+
+    private JLabel Name;
+    private JButton backButton;
+    private JButton NRF_bckbtn;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+        }
+    }
+
--- Activity0/src/views/NameResultPanel.java
+++ Activity0/src/views/NameResultPanel.java
@@ -5,14 +5,15 @@ import javax.swing.*;
 public class NameResultPanel {
     private JPanel mainPanel;
     private JComboBox NR_dropdown;
-    private JButton NR_backbutton;
+    private JButton NR_bckbtn;
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
 
     public JButton getNR_backbutton() {
-        return NR_backbutton;
+
+        return NR_bckbtn;
     }
 
     private void createUIComponents() {
