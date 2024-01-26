b5e6ca2807b5e87c47c7560c07087f2447e8360a
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 20:53:33 +0800

    + added: PlayerMetricsPanel.java

---
+++ Activity0/src/views/PlayerMetricsPanel.java
@@ -0,0 +1,46 @@
+package views;
+
+import javax.swing.*;
+
+public class PlayerMetricsPanel {
+    private JLabel playerNameLabel;
+    private JLabel idValue;
+    private JLabel sportValue;
+    private JLabel countryValue;
+    private JLabel goldValue;
+    private JLabel silverValue;
+    private JLabel bronzeValue;
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JLabel getPlayerNameLabel() {
+        return playerNameLabel;
+    }
+
+    public JLabel getCountryValue() {
+        return countryValue;
+    }
+
+    public JLabel getBronzeValue() {
+        return bronzeValue;
+    }
+
+    public JLabel getGoldValue() {
+        return goldValue;
+    }
+
+    public JLabel getIdValue() {
+        return idValue;
+    }
+
+    public JLabel getSilverValue() {
+        return silverValue;
+    }
+
+    public JLabel getSportValue() {
+        return sportValue;
+    }
+}
