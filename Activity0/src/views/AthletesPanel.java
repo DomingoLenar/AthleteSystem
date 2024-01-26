121a149229fa114a5bd5b67773fb102505afd895
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 20:51:50 +0800

    + updated: AthletesPanel.java

--- Activity0/src/views/AthletesPanel.java
+++ Activity0/src/views/AthletesPanel.java
@@ -2,13 +2,19 @@ package views;
 
 import javax.swing.*;
 
-public class PlayerInformationPanel {
+public class AthletesPanel {
     private JPanel mainPanel;
     private JComboBox PI_dropdown;
     private JButton PI_backbtn;
+    private String options[];
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+        PI_dropdown = new JComboBox();
+    }
+
+    public JComboBox getPI_dropdown() {
+        return PI_dropdown;
     }
 
     public JPanel getMainPanel() {
@@ -17,5 +23,11 @@ public class PlayerInformationPanel {
 
     public JButton getPI_backbtn() {
         return PI_backbtn;
+    }
+
+    public void populatePI_dropdown(String[] listOfPlayers) {
+        for (String players : listOfPlayers) {
+            PI_dropdown.addItem(players);
+        }
     }
 }
