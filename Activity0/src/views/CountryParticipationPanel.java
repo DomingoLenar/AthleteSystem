4a4b55913a5cff78ad6e532fe3de050bf1220d76
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 23:56:17 +0800

    + updated: CountryParticipationPanel.java

--- Activity0/src/views/CountryParticipationPanel.java
+++ Activity0/src/views/CountryParticipationPanel.java
@@ -1,6 +1,7 @@
 package views;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class CountryParticipationPanel {
     private JPanel mainPanel;
@@ -13,5 +14,20 @@ public class CountryParticipationPanel {
 
     public JButton getCP_backbtn() {
         return CP_backbtn;
+    }
+
+    public JComboBox getCP_dropdown() {
+        return CP_dropdown;
+    }
+
+    public void populateCP_dropdown(ArrayList<String> countries) {
+        for (String country : countries) {
+            CP_dropdown.addItem(country);
+        }
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        CP_dropdown = new JComboBox();
     }
 }
