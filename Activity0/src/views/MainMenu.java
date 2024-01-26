e337c95613c233720d512f103a3f3d463b9f43c8
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 11:11:20 +0800

    + partial user action in MainMenu.java

--- Activity0/src/views/MainMenu.java
+++ Activity0/src/views/MainMenu.java
@@ -1,17 +1,45 @@
 package views;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class MainMenu {
-    private JComboBox comboBox1;
+    private JComboBox dropdown;
     private JPanel mainPanel;
 
-    public JComboBox getComboBox1() {
-        return comboBox1;
+    public JComboBox getDropdown() {
+        return dropdown;
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        String options[] = {"Individual Player Information", "Team Information", "Game Participation", "Sports Performance"};
+        dropdown = new JComboBox(options);
+
+        dropdown.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+
+                    if (e.getItem().toString().equals(options[0]))
+                        System.out.println("1");
+                    if (e.getItem().toString().equals(options[1]))
+                        System.out.println("2");
+                    if (e.getItem().toString().equals(options[2]))
+                        System.out.println("3");
+                    if (e.getItem().toString().equals(options[3]))
+                        System.out.println("4");
+                }
+//                } else if (e.getStateChange() == ItemEvent.DESELECTED) {
+//                    // Item deselected
+//                    System.out.println("Deselected Item: " + e.getItem());
+//                }
+            }
+        });
+    }
 }
