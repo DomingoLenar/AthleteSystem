7cd2ca60bf838206db13155d8db7edf7d4c9411f
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 23:56:27 +0800

    + updated: CountryResultPanel.java

--- Activity0/src/views/CountryResultPanel.java
+++ Activity0/src/views/CountryResultPanel.java
@@ -5,19 +5,26 @@ import javax.swing.*;
 public class CountryResultPanel {
 
     private JPanel mainPanel;
-    private JButton CR_bckbtn;
+    private JButton CR_backbtn;
+    private JLabel countryValue;
+    private JLabel countryLabel;
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
-    public JButton getCR_bckbtn(){
-        return getCR_bckbtn();
-    }
 
-    public JButton getCP_backbtn() {
-        return CR_bckbtn;
+    public JButton getCR_backbtn() {
+        return CR_backbtn;
     }
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public JLabel getCountryValue() {
+        return countryValue;
+    }
+
+    public JLabel getCountryLabel() {
+        return countryLabel;
     }
 }
