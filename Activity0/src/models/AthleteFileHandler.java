b5b581e721ed2b8162e2409761af304f1ad0dab7
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 20:50:11 +0800

    + set the datatype of id,sex,age,height,weight into object of String

--- Activity0/src/models/AthleteFileHandler.java
+++ Activity0/src/models/AthleteFileHandler.java
@@ -4,15 +4,12 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class AthleteFileHandler implements FileHandlerInterface<AthleteModel> {
     private Scanner scanner = null;
 
     public AthleteFileHandler(){
 
     }
-
-
     public ArrayList<AthleteModel> readCSVFile(String filePath){
         try {
             File file = new File(filePath);
@@ -23,20 +20,25 @@ public class AthleteFileHandler implements FileHandlerInterface<AthleteModel> {
             ArrayList<AthleteModel> athletes = new ArrayList<>();
             while(scanner.hasNext()){
                 String[] rawData = scanner.nextLine().split(",");
-                int id = Integer.parseInt(rawData[0]);
+//                int id = Integer.parseInt(rawData[0].replaceAll("\"", ""));
+                String id1 = rawData[0];
                 String name = rawData[1];
-                char sex = rawData[2].charAt(0);
-                int age = Integer.parseInt(rawData[3]);
-                int height = Integer.parseInt(rawData[4]);
-                int weight = Integer.parseInt(rawData[5]);
+//                char sex = rawData[2].charAt(0);
+                String sex1 = rawData[2];
+//                int age = Integer.parseInt(rawData[3]);
+                String age1 = rawData[3];
+//                int height = Integer.parseInt(rawData[4]);
+                String height1 =rawData[4];
+//                int weight = Integer.parseInt(rawData[5]);
+                String weight1 = rawData[5];
                 String team = rawData[6];
                 String games = rawData[8];
                 String city = rawData[11];
                 String sport = rawData[12];
                 String medal = rawData[14];
-                athletes.add(new AthleteModel(id, name, sex, age, height, weight, team, games, city, sport, medal));
+                athletes.add(new AthleteModel(id1, name, sex1, age1, height1, weight1, team, games, city, sport, medal));
             }
-            return null;
+            return athletes;
         }catch(FileNotFoundException fileNotFound){
             fileNotFound.printStackTrace();
         }
