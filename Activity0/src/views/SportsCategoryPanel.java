f6a464ac88a9fcb4c653b05b76be60b62ca8e0b0
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 14:03:06 +0800

    + added: SportsCategoryPanel.java

---
+++ Activity0/src/views/SportsCategoryPanel.java
@@ -0,0 +1,17 @@
+package views;
+
+import javax.swing.*;
+
+public class SportsCategoryPanel {
+    private JPanel mainPanel;
+    private JComboBox SC_dropdown;
+    private JButton SC_backbtn;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getSC_backbtn() {
+        return SC_backbtn;
+    }
+}
