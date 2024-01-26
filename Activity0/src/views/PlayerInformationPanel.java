e0ea6c6ed6664f4ed616664cef69682ba2719151
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 14:02:59 +0800

    + added: PlayerInformationPanel.java

---
+++ Activity0/src/views/PlayerInformationPanel.java
@@ -0,0 +1,21 @@
+package views;
+
+import javax.swing.*;
+
+public class PlayerInformationPanel {
+    private JPanel mainPanel;
+    private JComboBox PI_dropdown;
+    private JButton PI_backbtn;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getPI_backbtn() {
+        return PI_backbtn;
+    }
+}
