814f353c8caf5fa0b56bdfa74f176889a8b21c03
Georcelle <2234664@slu.edu.ph>
2024-01-26 21:46:40 +0800

    Country Result Panel

---
+++ Activity0/src/views/CountryResultPanel.java
@@ -0,0 +1,20 @@
+package views;
+
+import javax.swing.*;
+
+public class CountryResultPanel {
+
+    private JPanel mainPanel;
+    private JButton CR_bckbtn;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+    public JButton getCR_bckbtn(){
+        return getCR_bckbtn();
+    }
+
+    public JButton getCP_backbtn() {
+        return CR_bckbtn;
+    }
+}
