d91f07a12f8d3ef6dfe0f4dfd345efc37b22320d
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 14:02:49 +0800

    + added: CountryParticipationPanel.java

---
+++ Activity0/src/views/CountryParticipationPanel.java
@@ -0,0 +1,17 @@
+package views;
+
+import javax.swing.*;
+
+public class CountryParticipationPanel {
+    private JPanel mainPanel;
+    private JComboBox CP_dropdown;
+    private JButton CP_backbtn;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getCP_backbtn() {
+        return CP_backbtn;
+    }
+}
