a4e33a9142bf5431a31c89fedbbdc936ac927bf8
Lenar R. Domingo <2233894@slu.edu.ph>
2024-01-26 21:33:34 +0800

    + MedalResult Panel + SportResult Panel + SportResultFound Panel

---
+++ Activity0/src/views/MedalResult.java
@@ -0,0 +1,20 @@
+package views;
+
+import javax.swing.*;
+
+public class MedalResult {
+    private JPanel mainPanel;
+    private JLabel Medal;
+    private JButton MR_backbutton;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getMR_backbutton() {
+        return MR_backbutton;
+    }
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ Activity0/src/views/SportResult.java
@@ -0,0 +1,21 @@
+package views;
+
+import javax.swing.*;
+
+public class SportResult {
+    private JPanel mainPanel;
+    private JComboBox SR_dropdown;
+    private JButton SR_backbutton;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JButton getSR_backbutton() {
+        return SR_backbutton;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
