cac5901278d3bb8641832a5770d88436d28af631
Lou Diamond <2233672@slu.edu.ph>
2024-01-26 20:51:30 +0800

    + updated: AthleteFilterInterface.java

--- Activity0/src/AthleteFilterInterface.java
+++ Activity0/src/AthleteFilterInterface.java
@@ -1,3 +1,5 @@
+import controllers.FilterController;
+import models.AthleteModel;
 import views.*;
 
 import javax.swing.*;
@@ -6,28 +8,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Objects;
 
 public class AthleteFilterInterface extends JFrame {
 
+    private FilterController filterController;
     private String selectFileID = "selectFile_ID";
     private final String chooseFileID = "chooseFile_id";
     private final String mainID = "main_id";
     private final String playerInformationID = "playerInformation_id";
+    private final String playerMetricID = "playerMetric_id";
     private final String countryParticipationID = "countryParticipation_id";
     private final String sportsCategoryID = "sports_id";
     private final String medalsID = "medals_id";
-    private final String totalGoldMedalsID = "gold_medals_id";
-    private final String totalSilverMedalsID = "silver_medals_id";
-    private final String totalBronzeMedalsID = "bronze_medals_id";
-    private final String totalNoMedalsID = "no_medals_id";
-    private SelectFile selectFile;
+    private final String medalMetricsID = "medal_metrics_id";
     private JPanel cardPanel;
+    private SelectFile selectFile;
     private ChooseFile chooseFile;
     private MainMenu mainMenu;
-    private PlayerInformationPanel playerInformationPanel;
+    private AthletesPanel playerInformationPanel;
+    private PlayerMetricsPanel playerMetricsPanel;
     private CountryParticipationPanel countryParticipationPanel;
     private SportsCategoryPanel sportsCategoryPanel;
     private MedalsPanel medalsPanel;
+    private MedalMetricsPanel medalMetricsPanel;
 
     public AthleteFilterInterface() {
         this.selectFile = new SelectFile();
@@ -60,7 +64,7 @@ public class AthleteFilterInterface extends JFrame {
         ((CardLayout)cardPanel.getLayout()).show(cardPanel, selectFileID);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 400);
+        setSize(800, 400);
         setVisible(true);
     }
 
@@ -73,10 +77,14 @@ public class AthleteFilterInterface extends JFrame {
     }
 
     private void initPanels() {
-        playerInformationPanel = new PlayerInformationPanel();
+        filterController = chooseFile.getFilterController();
+
+        playerInformationPanel = new AthletesPanel();
+        playerMetricsPanel = new PlayerMetricsPanel();
         countryParticipationPanel = new CountryParticipationPanel();
         sportsCategoryPanel = new SportsCategoryPanel();
         medalsPanel = new MedalsPanel();
+        medalMetricsPanel = new MedalMetricsPanel();
     }
 
     private void initButtons() {
@@ -100,19 +108,68 @@ public class AthleteFilterInterface extends JFrame {
 //                }
             }
         });
+        String[] PI_options = filterController.listOfPlayers();
+        playerInformationPanel.populatePI_dropdown(filterController.listOfPlayers());
+        playerInformationPanel.getPI_dropdown().addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    for (String option : PI_options) {
+                        if (e.getItem().toString().equals(option)){
+                            String[] temp_arr = option.split(",");
+                            playerMetricsPanel.getIdValue().setText(temp_arr[0]);
+                            playerMetricsPanel.getPlayerNameLabel().setText(temp_arr[5]);
+                            playerMetricsPanel.getSportValue().setText(temp_arr[9]);
+                            playerMetricsPanel.getCountryValue().setText(temp_arr[6]);
+
+                            if (!Objects.equals(temp_arr[10], "NA")){
+                                switch (temp_arr[10]){
+                                    case "Gold":
+                                        playerMetricsPanel.getGoldValue().setText(String.valueOf(1));
+                                        break;
+                                    case "Silver":
+                                        playerMetricsPanel.getSilverValue().setText(String.valueOf(1));
+                                        break;
+                                    case "Bronze":
+                                        playerMetricsPanel.getBronzeValue().setText(String.valueOf(1));
+                                        break;
+                                }
+                            }
+
+                            changeScreen(playerMetricID);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
         String medalsOption[] = medalsPanel.getOptions();
         medalsPanel.getMedal_dropdown().addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED){
-                    if (e.getItem().toString().equals(medalsOption[0]))
-                        changeScreen(totalGoldMedalsID);
-                    if (e.getItem().toString().equals(medalsOption[1]))
-                        changeScreen(totalSilverMedalsID);
-                    if (e.getItem().toString().equals(medalsOption[2]))
-                        changeScreen(totalBronzeMedalsID);
-                    if (e.getItem().toString().equals(medalsOption[3]))
-                        changeScreen(totalNoMedalsID);
+                    changeScreen(medalMetricsID);
+                    if (e.getItem().toString().equals(medalsOption[0])) {
+                        medalMetricsPanel.getTotalMedalLabel().setText("Total Gold:");
+                        int value = filterController.clickedMedalType(medalsOption[0]);
+                        medalMetricsPanel.getTotalMedalValue().setText(String.valueOf(value));
+                    }
+                    if (e.getItem().toString().equals(medalsOption[1])) {
+                        medalMetricsPanel.getTotalMedalLabel().setText("Total Silver:");
+                        int value = filterController.clickedMedalType(medalsOption[1]);
+                        medalMetricsPanel.getTotalMedalValue().setText(String.valueOf(value));
+                    }
+                    if (e.getItem().toString().equals(medalsOption[2])) {
+                        medalMetricsPanel.getTotalMedalLabel().setText("Total Bronze:");
+                        int value = filterController.clickedMedalType(medalsOption[2]);
+                        medalMetricsPanel.getTotalMedalValue().setText(String.valueOf(value));
+                    }
+                    if (e.getItem().toString().equals(medalsOption[3])) {
+                        medalMetricsPanel.getTotalMedalLabel().setText("Total No Medals:");
+                        int value = filterController.clickedMedalType("NA");
+                        medalMetricsPanel.getTotalMedalValue().setText(String.valueOf(value));
+                    }
                 }
             }
         });
@@ -142,6 +199,11 @@ public class AthleteFilterInterface extends JFrame {
         });
     }
 
+    private void displayPlayerMetricsPanel() {
+
+    }
+
+
     private void changeScreen(String screen) {
         ((CardLayout)cardPanel.getLayout()).show(cardPanel, screen);
     }
@@ -157,5 +219,8 @@ public class AthleteFilterInterface extends JFrame {
         cardPanel.add(countryParticipationID, countryParticipationPanel.getMainPanel());
         cardPanel.add(sportsCategoryID, sportsCategoryPanel.getMainPanel());
         cardPanel.add(medalsID, medalsPanel.getMainPanel());
+        cardPanel.add(medalMetricsID, medalMetricsPanel.getMainPanel());
+        cardPanel.add(playerMetricID, playerMetricsPanel.getMainPanel());
+
     }
 }
