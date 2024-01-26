bda82645bc13e2f2cb304be68ecc3c82c72d59b6
Georcelle <2234664@slu.edu.ph>
2024-01-26 21:22:30 +0800

    added null labels for results

--- Activity0/src/views/NameResultFoundPanel.java
+++ Activity0/src/views/NameResultFoundPanel.java
@@ -3,14 +3,21 @@ package views;
 import javax.swing.*;
 
 public class NameResultFoundPanel {
-        private JPanel mainPanel;
+    private JPanel mainPanel;
 
     private JLabel Name;
-    private JButton backButton;
+
     private JButton NRF_bckbtn;
 
-    public JPanel getMainPanel() {
+    private JPanel getMainPanel() {
         return mainPanel;
-        }
     }
+    private JButton NRF_bckbtn() {
+        return NRF_bckbtn;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
 
