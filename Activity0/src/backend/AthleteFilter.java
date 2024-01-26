08a5b911b7bf01e7311e39c0894c8e6544f1826d
Lestat <2233915@slu.edu.ph>
2024-01-26 11:37:43 +0800

    +documentation

--- Activity0/src/backend/AthleteFilter.java
+++ Activity0/src/backend/AthleteFilter.java
@@ -6,6 +6,12 @@ import java.util.stream.Collectors;
 
 public class AthleteFilter {
 
+    /**
+     * This method filters an array list of AthleteModel by the athletes name
+     * @param arrayList     ArrayList of AthleteModel
+     * @param playerName    Name of the player to be used as a filter in the ArrayList
+     * @return              Returns a filtered List of AthleteModel
+     */
     public List<AthleteModel> filterByName(ArrayList<AthleteModel> arrayList, String playerName){
         List<AthleteModel> filtered = null;
         filtered = arrayList.stream()
@@ -15,6 +21,12 @@ public class AthleteFilter {
         return filtered;
     }
 
+    /**
+     * This method filters an array list of AthleteModel by the name of a Team
+     * @param arrayList     ArrayList of AthleteModel to be filtered
+     * @param teamName      String of team name to be used to filter the ArrayList
+     * @return              Returns a List of AthleteModel that has been filtered using teamName
+     */
     public List<AthleteModel> filterByTeam(ArrayList<AthleteModel> arrayList, String teamName){
         List<AthleteModel> filtered = null;
         filtered = arrayList.stream()
@@ -23,6 +35,12 @@ public class AthleteFilter {
         return filtered;
     }
 
+    /**
+     * This method filters an ArrayList of AthleteModel by games
+     * @param arrayList     ArrayList of AthleteModel to be filtered
+     * @param game          String game to be used as a filter for the ArrayList
+     * @return              Returns a List of AthleteModel that has been filtered using game
+     */
     public List<AthleteModel> filterByGames(ArrayList<AthleteModel> arrayList, String game){
         List<AthleteModel> filtered = null;
         filtered = arrayList.stream()
@@ -31,6 +49,12 @@ public class AthleteFilter {
         return filtered;
     }
 
+    /**
+     *This method filters an ArrayList of AthleteModel by sport
+     * @param arrayList     ArrayList of AthleteModel to be filtered
+     * @param sport         String of sport to be used as a filter for the ArrayList
+     * @return              Returns a List of AthleteModel that has been filtered using sport
+     */
     public List<AthleteModel> filterBySport(ArrayList<AthleteModel> arrayList, String sport){
         List<AthleteModel> filtered = null;
         filtered = arrayList.stream()
@@ -39,6 +63,11 @@ public class AthleteFilter {
         return filtered;
     }
 
+    /**
+     *This method filters an ArrayList of AthleteModel and takes all the medal winners
+     * @param arrayList     ArrayList of AthleteModel to be filtered
+     * @return              Returns a List of List that with the index being List of Gold winners, followed by Silver and Bronze
+     */
     public List<List<AthleteModel>> filterByMedal(ArrayList<AthleteModel> arrayList){
         ArrayList<List<AthleteModel>> listOfMedals = new ArrayList<>();
 
@@ -47,9 +76,25 @@ public class AthleteFilter {
                 .collect(Collectors.toList());
         listOfMedals.add(goldMedals);
 
+        List<AthleteModel> silverMedals = arrayList.stream()
+                .filter(athleteModel -> athleteModel.getMedal().equalsIgnoreCase("silver"))
+                .collect(Collectors.toList());
+        listOfMedals.add(silverMedals);
+
+        List<AthleteModel> bronzeMedals = arrayList.stream()
+                .filter(athleteModel -> athleteModel.getMedal().equalsIgnoreCase("bronze"))
+                .collect(Collectors.toList());
+        listOfMedals.add(bronzeMedals);
+
         return listOfMedals.stream().toList();
     }
 
+    /**
+     *A method that filters an ArrayList of AthleteModel by the medal given
+     * @param arrayList     ArrayList of AthleteModel to be filtered
+     * @param medalType     String of medal to be used as a filter (Gold, Silver, Bronze)
+     * @return              Returns a List of AthleteModel that has a given medal won
+     */
     public List<AthleteModel> filterByMedal(ArrayList<AthleteModel> arrayList, String medalType){
         List<AthleteModel> filtered = null;
 
