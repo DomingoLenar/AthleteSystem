d190edfc9ef3426239e68ca5bae0cf484620fb0d
Lou Diamond <2233672@slu.edu.ph>
2024-01-27 21:18:20 +0800

    + updated: MedalMetricsPanel.java

--- Activity0/src/views/MedalMetricsPanel.java
+++ Activity0/src/views/MedalMetricsPanel.java
@@ -6,6 +6,11 @@ public class MedalMetricsPanel {
     private JLabel totalMedalLabel;
     private JLabel totalMedalValue;
     private JPanel mainPanel;
+    private JButton MM_backbtn;
+
+    public JButton getMM_backbtn() {
+        return MM_backbtn;
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
